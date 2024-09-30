ac4f863a9f0fd8c70264ea85b4a874cd8ede3a4c
Cho0924 <102133814+Cho0924@users.noreply.github.com>
2024-09-30 15:56:35 +0900

    finished EncoderBlock

--- model.py
+++ model.py
@@ -45,7 +45,7 @@ class PositionalEncoding(nn.Module):
         
 
 # In Layer Normalization, every token vector is normalized with its own mean and variance
-class LaterNormalization(nn.Module):
+class LayerNormalization(nn.Module):
     def __init__(self, eps: float = 10**-6) -> None: # eps for avoiding division by 0 or a too small number
         super().__init__()
         self.eps = eps
@@ -58,4 +58,93 @@ class LaterNormalization(nn.Module):
         mean = x.mean(dim = -1, keepdim=True) # calculate the mean of #d_model values (-> why dim = -1)
         std = x.std(dim = -1, keepdim=True) # want to keep the dimension so that it is compatible with input x
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+        
+    def forward(self, x):
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+    
+
+class MultiHeadAttentionBlock(nn.Module):
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        self.dropout = dropout
+        self.d_k = d_model // h
+        assert self.d_model % self.h == 0, "d_model is not divisible by h"
+        
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_o = nn.Linear(d_model, d_model)
+        
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+        
+        attention_score = (query @ key.transform(-2, -1)) / math.sqrt(d_k) # (Batch, h, seq_len, seq_len), shows the relations between each two words
+        if mask is not None:
+            attention_score.masked_fill_(mask == 0, -1e09)
+        attention_score = attention_score.softmax(dim = -1)
+        if dropout is not None:
+            attention_score = dropout(attention_score)
+        
+        # return attention_scores for visualization purpose
+        return (attention_score @ value), attention_score
+        
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (Batch, seq_len, d_model) => (Batch, seq_len, d_model)
+        key = self.w_k(k) # (Batch, seq_len, d_model) => (Batch, seq_len, d_model)
+        value = self.w_v(v) # (Batch, seq_len, d_model) => (Batch, seq_len, d_model)
+        
+        # split query, key, value, into different heads
+        # (Batch, seq_len, d_model) => (Batch, seq_len, h, d_k) => (Batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+        
+        # x is the value we use for calculation, self.attention_scores is for visualization
+        # x.size = (Batch, h, seq_len, d_k)
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+        
+        # (Batch, h, seq_len, d_k) => (Batch, seq_len, h, d_k) => (Batch, seq_len, d_model)
+        # Concatenate different heads. contiguous() is for placing the data of multiple heads contiguously in the memory for concatenation
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+        return self.w_o(x)
+        
+        
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+        
+    def forward(self, x, sublayer):
+        # even though in the paper, normalization is done after passing the input to sublayer
+        # in many implementation normalization is done first, so we stick to those here
+        # Also, recall that sublayer(x) automatically calls the forward method in the sublayer class
+        return x + self.dropout(sublayer(self.norm(x))) 
+
+class EncoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attetion_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)]) # we can call a class inside another class 
+        
+    def forward(self, x, src_mask):
+        # we pass lambda function as the second argument since forward method in ResidualConnection class requires a functio as its second argument
+        # src_mask is for preserving the auto-regressive property (refer to the paper)
+        # we cannot simply pass self.self_attention_block as the second argument, since this equals to doing self.self_attention_block(self.norm(x)) is you see line 133
+        x = self.residual_connections[0](x, lambda x: self.self_attetion_block(x, x, x, src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+        
         
